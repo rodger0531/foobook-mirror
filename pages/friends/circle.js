926d341d7158d40a42c6d32d8db0764b749810d5
Rodger <rodger0531@gmail.com>
2014-03-21 14:11:12 +0000

    started on friends, refined header bar

---
+++ pages/friends/circle.js
@@ -0,0 +1,93 @@
+$("document").ready(function()
+{
+    Session.set('user_id',1);
+    ajax(
+        'pages/friends/viewCircle',                       // PHP file to call on.
+        {user_id: Session.get('user_id')},  // User_id from session goes here
+        readListSuccess                     // Callback method to use on success.
+    );
+
+    $("#addCircle").on('submit', function(event)
+    {
+        event.preventDefault();
+        ajax(
+            'pages/friends/createCircle',                        // PHP file to call on.
+            {owner_id: Session.get('user_id'),circle_name:document.forms["addCircle"]["circle_name"].value},    // Data from serialised form with user input.
+            function success(){alert("Add circle successfully")
+            location.reload();}                         // Callback method to use on success.
+        );
+    });
+
+    $("#addFriendToCircle").on('submit', function(event)
+        {
+            event.preventDefault();
+            ajax(
+                'pages/friends/addFriendToCircle',                        // PHP file to call on.
+                {friend_id:document.forms["addFriendToCircle"]["friend_id"].value,circle_id: $("#circleList").val()},    // Data from serialised form with user input.
+                function success(data){
+                    alert("Added friend into circle successfully");
+                    loadList($("#circleList").val());
+                }                         // Callback method to use on success.
+            );
+        });
+
+    $("#circleList").on('change',function(){
+        loadList($(this).val());
+    });
+
+    $("#deleteCircle").on('click',function(){
+        ajax(
+            'pages/friends/removeCircle',                        // PHP file to call on.
+            {circle_id: $("#circleList").val()},    // Data from serialised form with user input.
+            function success(){
+                alert("Deleted circle successfully");
+                location.reload();
+            }                         // Callback method to use on success.
+        );
+    });
+
+    $("#friendList").on("click",".circleFriend",function(){
+        ajax(
+            'pages/friends/removeFriendFromCircle',                        // PHP file to call on.
+            {friend_id:$(this).val(),circle_id: $("#circleList").val()},    // Data from serialised form with user input.
+            function success(){
+                alert("Removed friend from circle successfully");
+                loadList($("#circleList").val());
+            }                         // Callback method to use on success.
+        );
+    });
+
+});
+
+function readListSuccess(data)
+{
+    $("#circleList").append('<option value = NULL > Select a circle </option>')
+    data.forEach(function(element){
+        $("#circleList").append('<option value ='+element.circle_id+'>'+element.circle_name+'</option>')
+    })
+}
+
+
+
+function loadList(circle_id){
+    if (circle_id!=="NULL"){
+        ajax(
+            'viewCircleFriend',                       // PHP file to call on.
+            {circle_id: circle_id},  // User_id from session goes here
+            loadListSuccess                    // Callback method to use on success.
+        );
+    }
+        
+}
+
+function loadListSuccess(friends){
+    $("#friendList").html("");
+    if (friends['outcome']===1){
+        // $("#friendList").append();
+        friends['response'].forEach(function(element){
+            $("#friendList").append('<button type="button" value='+element.user_id+' class="circleFriend">DELETE</button>'+element.first_name + ' ' + element.last_name+'</br>');
+        })
+    }else{
+        $("#friendList").html("");
+    }
+}
---
+++ pages/friends/viewCircleFriend.php
@@ -0,0 +1,45 @@
+<?php
+
+include '../../functions/abstract/query.php';
+$circle_id = $_POST['circle_id'];
+// $circle_id = 1;
+$action = 2;
+// Define a SQL query so that server can insert a friend into a circle.
+$query = "
+SELECT user.first_name,user.last_name,user.user_id
+FROM user
+JOIN friend_circle
+ON friend_circle.friend_id = user.user_id
+WHERE circle_id = :circle_id
+";
+
+// Define the parameters of the query depending on the information the user input.
+$params =
+array(
+	'circle_id'=> $circle_id
+);
+
+$result = query($action, $query, $params);
+
+if ($result['outcome'] === 0)
+{
+	if ($result['response'] === 201)
+	{
+		$result['response'] = "Query could not be executed!";
+	}
+	elseif ($result['response'] === 202)
+	{
+		$result['response'] = "There is no account associated with the given circle_id!";
+	}
+	elseif ($result['response'] === 203)
+	{
+		$result['response'] = "Server error!";
+	}
+	echo json_encode($result);
+}
+elseif ($result['outcome'] === 1)
+{
+
+	echo json_encode($result);
+}
+?>
